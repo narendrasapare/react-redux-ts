f585b3a38aa4ec540378462de428dce60419a36f
narendrasapare <narendra.sapare@gmail.com>
2020-09-02 10:52:47 +0530

    Functional Components with tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+// This is to pass props
 interface AppProps {
   color?: string;
 }
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };
 
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
+const App = (props: AppProps): JSX.Element => {
+  return <div>{props.color}</div>;
+};
+// This is to pass state
+// interface AppState {
+//   counter: number;
+// }
+// // Below is notation of class component as props and state are readonly we added interface
+// class App extends React.Component<AppProps, AppState> {
+//   constructor(props: AppProps) {
+//     super(props);
 
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-      </div>
-    );
-  }
-}
+//     this.state = { counter: 0 };
+//   }
+
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
+
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>Increment</button>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//       </div>
+//     );
+//   }
+// }
 
 ReactDOM.render(<App color="red" />, document.querySelector("#root"));
